6f82ff7a48fe39dd337fff85d2be2dd6a76ed115
denise <dpbaldon@up.edu.ph>
2024-03-07 15:53:11 +0800

    chore: modified request.js

--- request.js
+++ request.js
@@ -1,6 +1,6 @@
 import needle from 'needle';
 
-const newBook = {
+const book = {
     bookname: 'The Little Prince',
     isbn: '978-0156012195',
     author: 'Antoine Saint-Exupery',
@@ -11,10 +11,10 @@ needle.post('http://localhost:3000/add-book', (err,res) => {
     console.log(res.body);
 });
 
-needle.post('http://localhost:3000/find-by-isbn-author', (err,res) => {
+needle.post('http://localhost:3000/find-by-isbn-author/book?isbn=978-0-7475-3269-9', (err,res) => {
     console.log(res.body);
 });
 
-needle.post('http://localhost:3000/find-by-author', (err, res) => {
+needle.post('http://localhost:3000/find-by-author/book?author=J.K Rowling', (err, res) => {
     console.log(res.body);  
 });
